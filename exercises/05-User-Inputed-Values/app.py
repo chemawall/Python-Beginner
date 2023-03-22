e04a142acae876902aea416b6b4694986b894c61
chemawall <jmmuro88@gmail.com>
2023-03-22 15:57:14 +0000

    primeros65

--- exercises/05-User-Inputed-Values/app.py
+++ exercises/05-User-Inputed-Values/app.py
@@ -1,4 +1,7 @@
 age = int(input('What is your age?\n'))
 # CHANGE THE CODE BELOW TO ADD 10 TO AGE
-
+age = age + 10
 print("Your age is: "+str(age))
+
+#con int() pasamos a entero lo que tengamos entre paréntesis
+#con str() pasamos a string (cadena) lo que tengamos entre paréntesis

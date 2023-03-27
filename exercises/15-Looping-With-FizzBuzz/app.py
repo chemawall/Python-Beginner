6284f75a8f513a8427688d4774f2ae03747023f7
chemawall <jmmuro88@gmail.com>
2023-03-27 04:42:00 +0000

    15

--- exercises/15-Looping-With-FizzBuzz/app.py
+++ exercises/15-Looping-With-FizzBuzz/app.py
@@ -1,13 +1,16 @@
 def fizz_buzz():
-    for i in range(101):
-        if i%3 == 0:
-            print("Fizz")
+    for i in range(1,101):
+        if i%5 == 0 and i%3 == 0:
+            print("FizzBuzz")
         elif i%5 == 0: 
             print("Buzz")
-        elif (i%5 == 0 and i%3 == 0):
-            print("FizzBuzz")
+        elif i%3 == 0:
+            print("Fizz")
         else:
             print(i)
 
 
 fizz_buzz()
+
+#errores: el orden importa, primero los mas restrictivos
+#vigilar el rango, el cero tb se cuenta si no empieza desde el 1
